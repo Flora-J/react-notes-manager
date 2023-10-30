2e0c2328aa41a2863be878a3be5ab28202775ae6
Flora <flora.julie88@gmail.com>
2023-10-30 16:24:21 +0100

    Validation of form + edit note

--- src/components/NoteForm/NoteFrom.jsx
+++ src/components/NoteForm/NoteFrom.jsx
@@ -1,56 +1,126 @@
 import { ButtonPrimary } from "components/ButtonPrimary/ButtonPrimary";
 import s from "./style.module.css";
 import { PencilFill, TrashFill } from "react-bootstrap-icons";
+import { useState } from "react";
+import { ValidatorService } from "services/form-validator";
+import { FieldError } from "components/FieldError/FieldError";
 
-export function NoteForm({ title }) {
+const VALIDATORS = {
+  title: (value) => {
+    return ValidatorService.min(value, 3) || ValidatorService.max(value, 20);
+  },
+  content: (value) => {
+    return ValidatorService.min(value, 3);
+  },
+};
+
+export function NoteForm({
+  isEditable = true,
+  note,
+  title,
+  onClickEdit,
+  onClickTrash,
+  onSubmit,
+}) {
+  const [formValues, setFormValues] = useState({
+    title: note?.title,
+    content: note?.content,
+  });
+  const [formErrors, setFormErrors] = useState({
+    title: note?.title ? undefined : "",
+    content: note?.content ? undefined : "",
+  });
+
+  function hasError() {
+    return Object.values(formErrors).some((error) => error !== undefined);
+  }
+
+  function updateFormValues(e) {
+    setFormValues({ ...formValues, [e.target.name]: e.target.value });
+    validate(e.target.name, e.target.value);
+  }
+
+  function validate(fieldName, fieldValue) {
+    setFormErrors({
+      ...formErrors,
+      [fieldName]: VALIDATORS[fieldName](fieldValue),
+    });
+  }
+
   const actionIcons = (
     <>
       <div className="col-1">
-        <PencilFill className={s.icon} />
+        {onClickEdit && <PencilFill onClick={onClickEdit} className={s.icon} />}
       </div>
       <div className="col-1">
-        <TrashFill className={s.icon} />
+        {onClickTrash && (
+          <TrashFill onClick={onClickTrash} className={s.icon} />
+        )}
       </div>
     </>
   );
 
   const titleInput = (
-    <>
+    <div className="mb-5">
       <label className="form-label" htmlFor="">
         Title
       </label>
-      <input type="text" name="title" className="form-control" />
-    </>
+      <input
+        onChange={updateFormValues}
+        type="text"
+        name="title"
+        className="form-control"
+        value={formValues.title}
+      />
+      <FieldError msg={formErrors.title} />
+    </div>
   );
 
   const contentInput = (
-    <>
+    <div className="mb-5">
       <label className="form-label" htmlFor="">
         Content
       </label>
-      <textarea type="text" name="content" className="form-control" rows="5" />
-    </>
+      <textarea
+        onChange={updateFormValues}
+        type="text"
+        name="content"
+        className="form-control"
+        row="5"
+        value={formValues.content}
+      />
+      <FieldError msg={formErrors.content} />
+    </div>
   );
 
   const submitButton = (
     <>
       <div className={s.submit_btn}>
-        <ButtonPrimary>Submit</ButtonPrimary>
+        <ButtonPrimary
+          isDisabled={hasError()}
+          onClick={() => onSubmit(formValues)}
+        >
+          Submit
+        </ButtonPrimary>
       </div>
     </>
   );
 
   return (
-    <div className={s.container}>
+    <form className={s.container}>
       <div className="row justify-content-space-between">
         <div className="col-10">
           <h2 className="mb-3">{title}</h2>
         </div>
         {actionIcons}
       </div>
-      <div className={`mb-3 ${s.title_input_container}`}>{titleInput}</div>
-      <div className={`mb-3`}>{contentInput}</div>
-      {submitButton}
-    </div>
+      <div className={`mb-3 ${s.title_input_container}`}>
+        {isEditable && titleInput}
+      </div>
+      <div className={"mb-3"}>
+        {isEditable ? contentInput : <pre>{note.content}</pre>}
+      </div>
+      {onSubmit && submitButton}
+    </form>
   );
 }
