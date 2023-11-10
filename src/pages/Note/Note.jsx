6384ecd9a5bd91d001ef396322201793f5802017
Flora JULIE <flora.julie@icloud.com>
2023-11-10 16:44:39 +0100

    project finished

--- src/pages/Note/Note.jsx
+++ src/pages/Note/Note.jsx
@@ -1,17 +1,31 @@
+import { NoteAPI } from "api/note-api";
 import { NoteForm } from "components/NoteForm/NoteFrom";
 import { useState } from "react";
-import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+import { updateNote, deleteNote } from "store/note/note-slice";
 
 export function Note(props) {
   const [isEditable, setIsEditable] = useState(false);
+  const dispatch = useDispatch();
   const { noteId } = useParams();
   const note = useSelector((store) =>
     store.NOTE.noteList.find((note) => note.id === noteId)
   );
+  const navigate = useNavigate();
 
-  function submit() {
+  async function submit(formValues) {
+    const updatedNote = await NoteAPI.update({ ...formValues, id: note.id });
+    dispatch(updateNote(updatedNote));
+    setIsEditable(false);
+  }
 
+  function deleteNote_(note) {
+    if (window.confirm("Supprimer la note ?")) {
+      NoteAPI.deleteById(note.id);
+      dispatch(deleteNote(note));
+      navigate("/");
+    }
   }
 
   return (
@@ -22,7 +36,7 @@ export function Note(props) {
           title={isEditable ? "Edit note" : note.title}
           note={note}
           onClickEdit={() => setIsEditable(!isEditable)}
-          onClickTrash={() => ""}
+          onClickTrash={() => deleteNote_(note)}
           onSubmit={isEditable && submit}
         />
       )}
